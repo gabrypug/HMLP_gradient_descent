f77be2d0b042dc9a1a0dcabe49a5623afacdbfb7
Gabriele Puglisi <56605530+gabrypug@users.noreply.github.com>
2025-06-07 13:39:32 +0200

    Example of main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    uint32_t n_h = atoi(argv[1]);
-    uint32_t n_th = atoi(argv[2]);
+    uint32_t n_h = 5;
+    uint32_t n_th = 3;
     HMLPL net;
     uint32_t num_layers{3};
     uint32_t num_hidden_neurons[num_layers-2] = {n_h};
@@ -16,17 +16,17 @@ int main(int argc, char *argv[]) {
     uint32_t num_thread = n_th;
     uint32_t num_epochs = 10000;
     net.set_HMLPL(0,num_layers,num_hidden_neurons,rate,momentum,0,num_thread,num_epochs, 0.000001, 0.00000001);
-    net.get_training_data("dati/ExpCH_input.txt","dati/ExpCH_target.txt");
+    net.get_training_data("dati/HL_input.txt","dati/HL_target.txt");
     net.init();
 
     // TRAINING
     net.train_neural_net();
     net.export_weight_vector();
 
-    // if (net.upload_weights_bias("dati/weights.txt","dati/bias.txt")) {
-    //     net.get_inputs_test("dati/HL_pred.txt");
-    //     net.test();
-    // }
+    if (net.upload_weights_bias("dati/weights.txt","dati/bias.txt")) {
+        net.get_inputs_test("dati/HL_pred.txt");
+        net.test();
+    }
 
     return 0;
 }
